cf7e4515feabe0e8d9bca055d10bb57b64597125
Jonathan Hansen <kriah0872@gmail.com>
2020-05-14 23:03:39 -0700

    changed port back for heroku

--- serve.js
+++ serve.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require("path");
 const app = express();
-const PORT =  8000
+const PORT =  process.env.PORT;
 var notes = require('./db/db.json')
 
 
