79a5541ef7715f00a8a91662735fd3adfd4d7933
Jonathan Hansen <kriah0872@gmail.com>
2020-05-15 23:41:36 -0700

    working and refactored

---
+++ routes/api-routes/api-routes.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const fs = require('fs')
+
+module.exports = function (app) {
+  app.get("/api/notes", (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if(err) throw err;
+      res.json(JSON.parse(data))
+    })
+  });
+  app.post("/api/notes", (req, res) => {
+    const newNote = req.body
+    
+    // Read db.json file
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      // Parse json data and store in var
+      var noteList = JSON.parse(data);
+    newNote.id = (req.body.title.split(" "))[0].toLowerCase() + (noteList.length + 1);
+      console.log(noteList)
+  
+      noteList.push(newNote);
+  
+      console.log(noteList)
+  
+      const notesString = JSON.stringify(noteList)
+  
+      fs.writeFile('./db/db.json',notesString, err => {
+        if (err) throw err;
+        console.log(`Note Added:${notesString}`)
+      })
+    })
+  });
+  app.get("/api/notes/:id", (req, res) => {
+    const idRequest = req.params.id
+    console.log(idRequest)
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if(err) throw err;
+      const query = data.filter(note => note.id === idRequest)
+      console.log(query)
+      res.json(query)
+    })
+  });
+  app.delete("/notes/api/notes/:id", (req, res) => {
+    console.log(req.params.id);
+    const paramId = req.params.id;
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var filteredNotes = JSON.parse(data).filter(note => note.id !== paramId)
+    fs.writeFile('./db/db.json',JSON.stringify(filteredNotes), err => {
+      if (err) throw err;
+      console.log(`Note deleted: ${paramId}`)
+    })
+  })
+  })
+};
--- serve.js
+++ serve.js
@@ -1,42 +1,21 @@
 const express = require('express');
-const path = require("path");
 const app = express();
-const PORT =  process.env.PORT;
-var notes = require('./db/db.json')
-
+// const PORT =  process.env.PORT;
+const PORT = 8000;
+const htmlRoutes = require('./routes/html-routes/html-routes');
+const apiRoutes = require('./routes/api-routes/api-routes')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
+htmlRoutes(app);
+apiRoutes(app);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-app.post("/api/notes" , (req, res) => {
-  const newNote  = req.body
-  newNote.id = (req.body.title.split(" "))[0].toLowerCase();
-  notes.push(newNote)
-  console.log(notes)
-});
-app.get("/api/notes/:id", (req, res) => {
-  const idRequest = req.params.id
-  console.log(idRequest)
-  const query = notes.filter(note => note.id === idRequest)
-  console.log(query)
-  res.json(query)
-});
-app.delete("/api/notes/:id", (req,res) => {
-  console.log(req.params.id)
-  notes = notes.filter(note => note.id !== req.params.id)
-  console.log(notes)
-})
 app.listen(PORT, () => {
   console.log("Server started")
 });
+
+
+
+
