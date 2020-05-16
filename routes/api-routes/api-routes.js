8b91a10c7ea86c8571eebdf813809889bdcd07f3
Jonathan Hansen <kriah0872@gmail.com>
2020-05-15 23:51:59 -0700

    try this

--- routes/api-routes/api-routes.js
+++ routes/api-routes/api-routes.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 module.exports = function (app) {
   app.get("/api/notes", (req, res) => {
-    fs.readFile('../../db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if(err) throw err;
       res.json(JSON.parse(data))
     })
@@ -12,7 +12,7 @@ module.exports = function (app) {
     const newNote = req.body
     
     // Read db.json file
-    fs.readFile('../../db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) throw err;
       // Parse json data and store in var
       var noteList = JSON.parse(data);
@@ -25,7 +25,7 @@ module.exports = function (app) {
   
       const notesString = JSON.stringify(noteList)
   
-      fs.writeFile('../../db/db.json',notesString, err => {
+      fs.writeFile('./db/db.json',notesString, err => {
         if (err) throw err;
         console.log(`Note Added:${notesString}`)
       })
@@ -34,7 +34,7 @@ module.exports = function (app) {
   app.get("/api/notes/:id", (req, res) => {
     const idRequest = req.params.id
     console.log(idRequest)
-    fs.readFile('../../db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if(err) throw err;
       const query = data.filter(note => note.id === idRequest)
       console.log(query)
@@ -44,10 +44,10 @@ module.exports = function (app) {
   app.delete("/notes/api/notes/:id", (req, res) => {
     console.log(req.params.id);
     const paramId = req.params.id;
-    fs.readFile('../../db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     var filteredNotes = JSON.parse(data).filter(note => note.id !== paramId)
-    fs.writeFile('../../db/db.json',JSON.stringify(filteredNotes), err => {
+    fs.writeFile('./db/db.json',JSON.stringify(filteredNotes), err => {
       if (err) throw err;
       console.log(`Note deleted: ${paramId}`)
     })
