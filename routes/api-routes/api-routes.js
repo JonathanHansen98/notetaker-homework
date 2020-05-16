89951a0259b7fa02822b287d0591e11b80220e02
Jonathan Hansen <kriah0872@gmail.com>
2020-05-16 00:09:30 -0700

    removed unnecessary console logs

--- routes/api-routes/api-routes.js
+++ routes/api-routes/api-routes.js
@@ -17,12 +17,9 @@ module.exports = function (app) {
       // Parse json data and store in var
       var noteList = JSON.parse(data);
     newNote.id = (req.body.title.split(" "))[0].toLowerCase() + (noteList.length + 1);
-      console.log(noteList)
   
-      noteList.push(newNote);
-  
-      console.log(noteList)
-  
+      noteList.push(newNote);  
+
       const notesString = JSON.stringify(noteList)
   
       fs.writeFile('./db/db.json',notesString, err => {
@@ -33,16 +30,13 @@ module.exports = function (app) {
   });
   app.get("/api/notes/:id", (req, res) => {
     const idRequest = req.params.id
-    console.log(idRequest)
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if(err) throw err;
       const query = data.filter(note => note.id === idRequest)
-      console.log(query)
       res.json(query)
     })
   });
   app.delete("/api/notes/:id", (req, res) => {
-    console.log(req.params.id);
     const paramId = req.params.id;
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
