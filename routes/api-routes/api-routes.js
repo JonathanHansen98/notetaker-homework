5cf8dccc386afe10fd1c75414a3db71148bd8c95
Jonathan Hansen <kriah0872@gmail.com>
2020-05-21 21:32:57 -0700

    converted to readFileSync/writeFileSync

--- routes/api-routes/api-routes.js
+++ routes/api-routes/api-routes.js
@@ -3,46 +3,35 @@ const fs = require('fs')
 
 module.exports = function (app) {
   app.get("/api/notes", (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if(err) throw err;
-      res.json(JSON.parse(data))
-    })
-  });
+    const data = fs.readFileSync('./db/db.json', 'utf8') 
+    res.json(JSON.parse(data))
+  
+  })
   app.post("/api/notes", (req, res) => {
     const newNote = req.body
     
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      var noteList = JSON.parse(data);
-      newNote.id = (req.body.title.split(" "))[0].toLowerCase() + (noteList.length + 1);
+    const data = fs.readFileSync('./db/db.json', 'utf8')
+    var noteList = JSON.parse(data);
+    newNote.id = (req.body.title.split(" "))[0].toLowerCase() + (noteList.length + 1);
   
-      noteList.push(newNote);  
-
-      const notesString = JSON.stringify(noteList)
+    noteList.push(newNote);  
   
-      fs.writeFile('./db/db.json',notesString, err => {
-        if (err) throw err;
-        console.log(`Note Added:${JSON.stringify(newNote)}`)
-      })
-    })
+    const notesString = JSON.stringify(noteList)
+    fs.writeFileSync('./db/db.json',notesString)
+    console.log(`Note Added:${JSON.stringify(newNote)}`)
+    
   });
   app.get("/api/notes/:id", (req, res) => {
     const idRequest = req.params.id
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if(err) throw err;
-      const query = data.filter(note => note.id === idRequest)
-      res.json(query)
-    })
+    const data = fs.readFileSync('./db/db.json', 'utf8')
+    const query = data.filter(note => note.id === idRequest)
+    res.json(query)
   });
   app.delete("/api/notes/:id", (req, res) => {
     const paramId = req.params.id;
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
+    const data = fs.readFileSync('./db/db.json', 'utf8');
     var filteredNotes = JSON.parse(data).filter(note => note.id !== paramId)
-    fs.writeFile('./db/db.json',JSON.stringify(filteredNotes), err => {
-      if (err) throw err;
-      console.log(`Note deleted: ${paramId}`)
+    fs.writeFileSync('./db/db.json',JSON.stringify(filteredNotes))
+    console.log(`Note deleted`)
     })
-  })
-  })
 };
